cb2230ce5af0f3a5b95f4cb806fc80c2f5dc2dc0
Fabian Hahner <hahfab1230@pass-consulting.com>
2018-09-13 15:11:52 +0200

    Initial commit

--- js/footer.js
+++
@@ -1,3 +0,0 @@
-var getImport = document.querySelector('#extfooter');
-var getContent = getImport.import.querySelector('#footer');
-document.body.appendChild(document.importNode(getContent, true));
--- js/header.js
+++
@@ -1,3 +0,0 @@
-var getImport = document.querySelector('#extheader');
-var getContent = getImport.import.querySelector('#header');
-document.body.appendChild(document.importNode(getContent, true));
